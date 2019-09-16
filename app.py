166c460cf2be2f98ffef6d5f02924ee6c7b5595c
NaokiYoshida <dw9t5j9@gmail.com>
2019-09-16 20:04:48 +0900

    create repository

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']="postgresql://localhost/mydb"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Content(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True)
+    content = db.Column(db.String(140), index=True)
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+@app.route('/')
+def index():
+    contents = Content.query.all()
+    return render_template('index.html', contents=contents)
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/post', methods=['POST'])
+def post():
+    if request.form['username'] and  request.form['content']:
+        newContent = Content(username=request.form['username'], content=request.form['content'])
+        db.session.add(newContent)
+        db.session.commit()
+        return render_template('result.html', username=request.form['username'], content=request.form['content'])   
+    else:
+        return render_template('error.html')
+
+@app.cli.command('initdb')
+def initdb_command():
+    db.create_all()
